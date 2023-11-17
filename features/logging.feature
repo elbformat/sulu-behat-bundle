6f0ff4f79f34ffda31891b7e0c66f2ebf39d005c
Ronny Gericke <ronny.gericke@format-h.com>
2023-11-17 10:04:20 +0100

    [FIX] non-existent references to BrowserContext in symfony behat bundle @1.5

--- features/logging.feature
+++
@@ -1,6 +0,0 @@
-Feature: Test if logging works as expected
-  Scenario: Warnings are logged
-    When I go to "/"
-    Then the page shows up
-    And the main logfile contains an info entry "Matched route"
-    And the main logfile doesn't contain any warning entries
--- src/Context/SuluAdminContext.php
+++ src/Context/SuluAdminContext.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace Elbformat\SuluBehatBundle\Context;
 
-use Elbformat\SymfonyBehatBundle\Context\BrowserContext as SymfonyBrowserContext;
+use Behat\Behat\Context\Context;
+use Elbformat\SymfonyBehatBundle\Browser\State;
+use Elbformat\SymfonyBehatBundle\Context\RequestTrait;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * @author Hannes Giesenow <hannes.giesenow@elbformat.de>
  */
-class BrowserContext extends SymfonyBrowserContext
+class SuluAdminContext implements Context
 {
+    use RequestTrait;
+
+    public function __construct(
+        protected State $state,
+        protected KernelInterface $kernel,
+    ) {
+
+    }
+
     /**
      * @Given I am logged in as admin
      */
